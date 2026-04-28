336a378fb17ed612c9c1fd2169115f744f97ef3c
Fred120-code <fredayemtsa@gmail.com>
2026-04-28 19:58:01 +0100

    fix: add new url client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import {
   SquareActivity,
 } from "lucide-react";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://health-data-collector-pi.vercel.app";
 
 function App() {
   const [data, setData] = useState([]);
